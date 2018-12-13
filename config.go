9a48cf9d619a48cfac240619c0a1982aeac2489e
jinbanglin <17golang@gmail.com>
2018-12-13 13:17:15 +0800

    TRACE

--- config.go
+++ config.go
@@ -1,39 +1,39 @@
 package log
 
 import (
-	"os"
+  "os"
 
-	"github.com/spf13/viper"
+  "github.com/spf13/viper"
 )
 
 type level = uint8
 type coreStatus = uint32
 
 const (
-	_DEBUG    level = iota + 1
-	_INFO
-	_WARN
-	_ERR
-	_DISASTER
+  _DEBUG    level = iota + 1
+  _INFO
+  _WARN
+  _ERR
+  _DISASTER
 )
 
 const (
-	B  = 1 << (10 * iota)
-	KB
-	MB
-	GB
-	TB
-	PB
+  B  = 1 << (10 * iota)
+  KB
+  MB
+  GB
+  TB
+  PB
 )
 const (
-	OUT_STDOUT = 0x1f
-	OUT_FILE   = 0x8b
+  OUT_STDOUT = 0x1f
+  OUT_FILE   = 0x8b
 )
 
 var (
-	coreDead    coreStatus = 2 //gLogger is dead
-	coreBlock   coreStatus = 0 //gLogger is block
-	coreRunning coreStatus = 1 //gLogger is running
+  coreDead    coreStatus = 2 //gLogger is dead
+  coreBlock   coreStatus = 0 //gLogger is block
+  coreRunning coreStatus = 1 //gLogger is running
 )
 
 var gSetOut = OUT_STDOUT
@@ -44,39 +44,40 @@ var gSetFilename = "moss"
 var gSetFilePath = getCurrentDirectory()
 var gSetLevel = _DEBUG
 var gSetPollerInterval = 500
+var gContextKey = "TRACE"
 
 func setupConfig() {
-	if value := viper.GetInt("log.bucketlen"); value > 0 {
-		gSetBucketLen = value
-	}
-	if value := viper.GetString("log.linkname"); value != "" {
-		gSetFilename = value
-	}
-	if value := viper.GetString("log.out"); value != "" {
-		switch value {
-		case "stdout":
-			gSetOut = OUT_STDOUT
-		case "file":
-			gSetOut = OUT_FILE
-		default:
-			gSetOut = OUT_STDOUT
-		}
-	}
-	if value := viper.GetString("log.filepath"); value != "" {
-		if !pathIsExist(value) &&gSetOut==OUT_FILE{
-			if err := os.Mkdir(value, os.ModePerm); err != nil {
-				panic(err)
-			}
-		}
-		gSetFilePath = value
-	}
-	if value := viper.GetInt("log.level"); value > 0 {
-		gSetLevel = level(value)
-	}
-	if value := viper.GetInt("log.maxsize"); value > 0 {
-		gSetMaxSize = value * MB
-	}
-	if value := viper.GetInt("log.interval"); value > 0 {
-		gSetPollerInterval = value
-	}
+  if value := viper.GetInt("log.bucketlen"); value > 0 {
+    gSetBucketLen = value
+  }
+  if value := viper.GetString("log.linkname"); value != "" {
+    gSetFilename = value
+  }
+  if value := viper.GetString("log.out"); value != "" {
+    switch value {
+    case "stdout":
+      gSetOut = OUT_STDOUT
+    case "file":
+      gSetOut = OUT_FILE
+    default:
+      gSetOut = OUT_STDOUT
+    }
+  }
+  if value := viper.GetString("log.filepath"); value != "" {
+    if !pathIsExist(value) && gSetOut == OUT_FILE {
+      if err := os.Mkdir(value, os.ModePerm); err != nil {
+        panic(err)
+      }
+    }
+    gSetFilePath = value
+  }
+  if value := viper.GetInt("log.level"); value > 0 {
+    gSetLevel = level(value)
+  }
+  if value := viper.GetInt("log.maxsize"); value > 0 {
+    gSetMaxSize = value * MB
+  }
+  if value := viper.GetInt("log.interval"); value > 0 {
+    gSetPollerInterval = value
+  }
 }
--- log.go
+++ log.go
@@ -1,125 +1,126 @@
 package log
 
 import (
-	"bufio"
-	"errors"
-	"fmt"
-	"os"
-	"os/signal"
-	"path"
-	"path/filepath"
-	"runtime"
-	"strconv"
-	"strings"
-	"sync"
-	"sync/atomic"
-	"syscall"
-	"time"
-	"github.com/jinbanglin/bytebufferpool"
-	"unsafe"
+  "bufio"
+  "errors"
+  "fmt"
+  "os"
+  "os/signal"
+  "path"
+  "path/filepath"
+  "runtime"
+  "strconv"
+  "strings"
+  "sync"
+  "sync/atomic"
+  "syscall"
+  "time"
+  "github.com/jinbanglin/bytebufferpool"
+  "unsafe"
+  "context"
 )
 
 var gLogger *Logger
 
 func init() {
-	setupLogger()
+  setupLogger()
 }
 
 func setupLogger() {
-	y, m, d := time.Now().Date()
-	gLogger = &Logger{
-		look:        uint32(coreDead),
-		fileName:    gSetFilename,
-		fileBufSize: gSetBufSize,
-		path:        filepath.Join(gSetFilePath, gSetFilename),
-		timestamp:   y*10000 + int(m)*100 + d*1,
-		fileMaxSize: gSetMaxSize,
-		bucket:      make(chan *bytebufferpool.ByteBuffer, gSetBucketLen),
-		closeSignal: make(chan string),
-		lock:        &sync.RWMutex{},
-		sigChan:     make(chan os.Signal),
-	}
+  y, m, d := time.Now().Date()
+  gLogger = &Logger{
+    look:        uint32(coreDead),
+    fileName:    gSetFilename,
+    fileBufSize: gSetBufSize,
+    path:        filepath.Join(gSetFilePath, gSetFilename),
+    timestamp:   y*10000 + int(m)*100 + d*1,
+    fileMaxSize: gSetMaxSize,
+    bucket:      make(chan *bytebufferpool.ByteBuffer, gSetBucketLen),
+    closeSignal: make(chan string),
+    lock:        &sync.RWMutex{},
+    sigChan:     make(chan os.Signal),
+  }
 }
 
 func SetupMossLog() {
-	setupConfig()
-	setupLogger()
-	if gSetOut == OUT_FILE {
-		go poller()
-	}
+  setupConfig()
+  setupLogger()
+  if gSetOut == OUT_FILE {
+    go poller()
+  }
 }
 
 func (l *Logger) loadCurLogFile() error {
-	l.link = filepath.Join(l.path, gSetFilename+".log")
-	actFileName, ok := isLinkFile(l.link)
-	if !ok {
-		return errors.New(l.link + "is not link file or not exist")
-	}
-	l.fileName = filepath.Join(l.path, actFileName)
-	f, err := openFile(l.fileName)
-	if err != nil {
-		return err
-	}
-	info, err := os.Stat(l.fileName)
-	if err != nil {
-		return err
-	}
-	t, err := time.Parse("2006.01.02.15:04:05", strings.TrimSuffix(actFileName, ".log"))
-	if err != nil {
-		fmt.Errorf("loadCurrentLogFile |err=%v", err)
-		return err
-	}
-	y, m, d := t.Date()
-	l.timestamp = y*10000 + int(m)*100 + d*1
-	l.file = f
-	l.fileActualSize = int(info.Size())
-	l.fileWriter = bufio.NewWriterSize(f, l.fileBufSize)
-	return nil
+  l.link = filepath.Join(l.path, gSetFilename+".log")
+  actFileName, ok := isLinkFile(l.link)
+  if !ok {
+    return errors.New(l.link + "is not link file or not exist")
+  }
+  l.fileName = filepath.Join(l.path, actFileName)
+  f, err := openFile(l.fileName)
+  if err != nil {
+    return err
+  }
+  info, err := os.Stat(l.fileName)
+  if err != nil {
+    return err
+  }
+  t, err := time.Parse("2006.01.02.15:04:05", strings.TrimSuffix(actFileName, ".log"))
+  if err != nil {
+    fmt.Errorf("loadCurrentLogFile |err=%v", err)
+    return err
+  }
+  y, m, d := t.Date()
+  l.timestamp = y*10000 + int(m)*100 + d*1
+  l.file = f
+  l.fileActualSize = int(info.Size())
+  l.fileWriter = bufio.NewWriterSize(f, l.fileBufSize)
+  return nil
 }
 
 func (l *Logger) createFile() (err error) {
-	if !pathIsExist(l.path) {
-		if err = os.MkdirAll(l.path, os.ModePerm); err != nil {
-			return
-		}
-	}
-	now := time.Now()
-	y, m, d := now.Date()
-	l.timestamp = y*10000 + int(m)*100 + d*1
-	l.fileName = filepath.Join(
-		l.path,
-		now.Format("2006.01.02.15:04:05")+".log")
-	f, err := openFile(l.fileName)
-	if err != nil {
-		return err
-	}
-	l.file = f
-	l.fileActualSize = 0
-	l.fileWriter = bufio.NewWriterSize(f, l.fileBufSize)
-	l.link = filepath.Join(l.path, gSetFilename+".log")
-	return createLinkFile(l.fileName, l.link)
+  if !pathIsExist(l.path) {
+    if err = os.MkdirAll(l.path, os.ModePerm); err != nil {
+      return
+    }
+  }
+  now := time.Now()
+  y, m, d := now.Date()
+  l.timestamp = y*10000 + int(m)*100 + d*1
+  l.fileName = filepath.Join(
+    l.path,
+    now.Format("2006.01.02.15:04:05")+".log")
+  f, err := openFile(l.fileName)
+  if err != nil {
+    return err
+  }
+  l.file = f
+  l.fileActualSize = 0
+  l.fileWriter = bufio.NewWriterSize(f, l.fileBufSize)
+  l.link = filepath.Join(l.path, gSetFilename+".log")
+  return createLinkFile(l.fileName, l.link)
 }
 
 func (l *Logger) sync() {
-	if l.lookRunning() {
-		l.fileWriter.Flush()
-	}
+  if l.lookRunning() {
+    l.fileWriter.Flush()
+  }
 }
 
 const fileMaxDelta = 100
 
 func (l *Logger) rotate() bool {
-	if !l.lookRunning() {
-		return false
-	}
-	y, m, d := time.Now().Date()
-	timestamp := y*10000 + int(m)*100 + d*1
-	if l.fileActualSize <= l.fileMaxSize-fileMaxDelta && timestamp <= l.timestamp {
-		return false
-	}
-	l.fileWriter.Flush()
-	closeFile(l.file)
-	return l.createFile() == nil
+  if !l.lookRunning() {
+    return false
+  }
+  y, m, d := time.Now().Date()
+  timestamp := y*10000 + int(m)*100 + d*1
+  if l.fileActualSize <= l.fileMaxSize-fileMaxDelta && timestamp <= l.timestamp {
+    return false
+  }
+  l.fileWriter.Flush()
+  closeFile(l.file)
+  return l.createFile() == nil
 }
 
 func (l *Logger) lookRunning() bool { return atomic.LoadUint32(&l.look) == uint32(coreRunning) }
@@ -129,160 +130,278 @@ func (l *Logger) lookDead() bool { return atomic.LoadUint32(&l.look) == uint32(c
 func (l *Logger) lookBlock() bool { return atomic.LoadUint32(&l.look) == uint32(coreBlock) }
 
 func (l *Logger) signalHandler() {
-	signal.Notify(l.sigChan, syscall.SIGTERM, syscall.SIGINT)
-	for {
-		select {
-		case sig := <-l.sigChan:
-			l.closeSignal <- "close"
-			fmt.Println("receive os signal is ", sig)
-			l.fileWriter.Flush()
-			closeFile(l.file)
-			atomic.SwapUint32(&l.look, uint32(coreDead))
-			close(l.bucket)
-			os.Exit(1)
-		}
-	}
+  signal.Notify(l.sigChan, syscall.SIGTERM, syscall.SIGINT)
+  for {
+    select {
+    case sig := <-l.sigChan:
+      l.closeSignal <- "close"
+      fmt.Println("receive os signal is ", sig)
+      l.fileWriter.Flush()
+      closeFile(l.file)
+      atomic.SwapUint32(&l.look, uint32(coreDead))
+      close(l.bucket)
+      os.Exit(1)
+    }
+  }
 }
 
 func (l *Logger) release(buf *bytebufferpool.ByteBuffer) { bytebufferpool.Put(buf) }
 
 func caller() string {
-	pc, f, l, _ := runtime.Caller(2)
-	funcName := runtime.FuncForPC(pc).Name()
-	return path.Base(f) + "/" + path.Base(funcName) + " [" + strconv.Itoa(l) + "] "
+  pc, f, l, _ := runtime.Caller(2)
+  funcName := runtime.FuncForPC(pc).Name()
+  return path.Base(f) + "/" + path.Base(funcName) + " [" + strconv.Itoa(l) + "] "
 }
 
 func print(buf *bytebufferpool.ByteBuffer) {
-	switch gSetOut {
-	case OUT_FILE:
-		gLogger.bucket <- buf
-	case OUT_STDOUT:
-		fmt.Print(buf.String())
-	default:
-		fmt.Print(buf.String())
-	}
+  switch gSetOut {
+  case OUT_FILE:
+    gLogger.bucket <- buf
+  case OUT_STDOUT:
+    fmt.Print(buf.String())
+  default:
+    fmt.Print(buf.String())
+  }
 }
 
 func Debugf(format string, msg ... interface{}) {
-	if gSetLevel > _DEBUG {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[DEBU] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
-	print(buf)
+  if gSetLevel > _DEBUG {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[DEBU] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
 }
 
 func Infof(format string, msg ... interface{}) {
-	if gSetLevel > _INFO {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[INFO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
-	print(buf)
+  if gSetLevel > _INFO {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[INFO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
 }
 
 func Warnf(format string, msg ... interface{}) {
-	if gSetLevel > _WARN {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[WARN] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
-	print(buf)
+  if gSetLevel > _WARN {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[WARN] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
 }
 
 func Errorf(format string, msg ... interface{}) {
-	if gSetLevel > _ERR {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[ERRO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
-	print(buf)
+  if gSetLevel > _ERR {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[ERRO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
 }
 
 func Fatalf(format string, msg ... interface{}) {
-	if gSetLevel > _DISASTER {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[FTAL] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
-	print(buf)
+  if gSetLevel > _DISASTER {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[FTAL] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
 }
 
 func Stackf(format string, msg ... interface{}) {
-	s := fmt.Sprintf(format, msg...)
-	s += "\n"
-	buf := make([]byte, 1<<20)
-	n := runtime.Stack(buf, true)
-	s += string(buf[:n])
-	s += "\n"
-	fmt.Println("[STAC][" + time.Now().Format("01/02/15:04:05") + "]" + "[" + caller() + "] ❀ " + s)
+  s := fmt.Sprintf(format, msg...)
+  s += "\n"
+  buf := make([]byte, 1<<20)
+  n := runtime.Stack(buf, true)
+  s += string(buf[:n])
+  s += "\n"
+  fmt.Println("[STAC][" + time.Now().Format("01/02/15:04:05") + "]" + "[" + caller() + "] ❀ " + s)
 }
 
 func Debug(msg ... interface{}) {
-	if gSetLevel > _DEBUG {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[DEBU] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintln(msg...)))
-	print(buf)
+  if gSetLevel > _DEBUG {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[DEBU] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
 }
 
 func Info(msg ... interface{}) {
-	if gSetLevel > _INFO {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[INFO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintln(msg...)))
-	print(buf)
+  if gSetLevel > _INFO {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[INFO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
 }
 
 func Warn(msg ... interface{}) {
-	if gSetLevel > _WARN {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[WARN] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintln(msg...)))
-	print(buf)
+  if gSetLevel > _WARN {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[WARN] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
 }
 
 func Error(msg ... interface{}) {
-	if gSetLevel > _ERR {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[ERRO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintln(msg...)))
-	print(buf)
+  if gSetLevel > _ERR {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[ERRO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
 }
 
 func Fatal(msg ... interface{}) {
-	if gSetLevel > _DISASTER {
-		return
-	}
-	buf := bytebufferpool.Get()
-	buf.Write(string2Byte("[FTAL] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
-	buf.Write(string2Byte(fmt.Sprintln(msg...)))
-	print(buf)
+  if gSetLevel > _DISASTER {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[FTAL] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
 }
 
 func Stack(msg ... interface{}) {
-	s := fmt.Sprintln(msg...)
-	s += "\n"
-	buf := make([]byte, 1<<20)
-	n := runtime.Stack(buf, true)
-	s += string(buf[:n])
-	s += "\n"
-	fmt.Println("[STAC][" + time.Now().Format("01/02/15:04:05") + "]" + "[" + caller() + "] ❀ " + s)
+  s := fmt.Sprintln(msg...)
+  s += "\n"
+  buf := make([]byte, 1<<20)
+  n := runtime.Stack(buf, true)
+  s += string(buf[:n])
+  s += "\n"
+  fmt.Println("[STAC][" + time.Now().Format("01/02/15:04:05") + "]" + "[" + caller() + "] ❀ " + s)
+}
+
+func Debugf2(ctx context.Context, format string, msg ... interface{}) {
+  if gSetLevel > _DEBUG {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[DEBU] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
+}
+
+func Infof2(ctx context.Context, format string, msg ... interface{}) {
+  if gSetLevel > _INFO {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[INFO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey +"="+ getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
+}
+
+func Warnf2(ctx context.Context, format string, msg ... interface{}) {
+  if gSetLevel > _WARN {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[WARN] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
+}
+
+func Errorf2(ctx context.Context, format string, msg ... interface{}) {
+  if gSetLevel > _ERR {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[ERRO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
+}
+
+func Fatalf2(ctx context.Context, format string, msg ... interface{}) {
+  if gSetLevel > _DISASTER {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[FTAL] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey +"="+ getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintf(format, msg...) + "\n"))
+  print(buf)
+}
+
+func Debug2(ctx context.Context, msg ... interface{}) {
+  if gSetLevel > _DEBUG {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[DEBU] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
+}
+
+func Info2(ctx context.Context, msg ... interface{}) {
+  if gSetLevel > _INFO {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[INFO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
+}
+
+func Warn2(ctx context.Context, msg ... interface{}) {
+  if gSetLevel > _WARN {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[WARN] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
+}
+
+func Error2(ctx context.Context, msg ... interface{}) {
+  if gSetLevel > _ERR {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[ERRO] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey + "="+getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
+}
+
+func Fatal2(ctx context.Context, msg ... interface{}) {
+  if gSetLevel > _DISASTER {
+    return
+  }
+  buf := bytebufferpool.Get()
+  buf.Write(string2Byte("[FTAL] " + time.Now().Format("01/02/15:04:05") + " " + caller() + "❀ "))
+  buf.Write(string2Byte(gContextKey +"="+ getContextValue(ctx) + " |"))
+  buf.Write(string2Byte(fmt.Sprintln(msg...)))
+  print(buf)
 }
 
 func string2Byte(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&s))
+  return *(*[]byte)(unsafe.Pointer(&s))
+}
+
+func getContextValue(ctx context.Context) string {
+  return ctx.Value(gContextKey).(string)
+}
+
+func SetContext(ctx context.Context, key, value string) context.Context {
+  return context.WithValue(ctx, key, value)
 }
